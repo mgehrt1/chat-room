72a500190f58058c6739558998f6220e64892063
Matthew Gehrt <mgehrt@hbs.net>
2023-07-17 16:00:19 -0500

    update

--- components/ChatRoom.tsx
+++ components/ChatRoom.tsx
@@ -1,12 +1,9 @@
-import { doc, setDoc } from "firebase/firestore";
-import { useCollectionData } from "react-firebase-hooks/firestore";
+import { collection } from "firebase/firestore";
 
 interface Props {
     roomId: string;
 }
 
 export default function ChatRoom({ roomId }: Props) {
-    // const messagesRef = firestore.collection("messages");
-
     return <h1>{roomId}</h1>;
 }
--- components/RoomSelector.tsx
+++ components/RoomSelector.tsx
@@ -1,3 +1,5 @@
+import { db } from "@/config/firebase";
+import { doc, getDoc } from "firebase/firestore";
 import { useState } from "react";
 
 export default function RoomSelector() {
@@ -6,22 +8,35 @@ export default function RoomSelector() {
     const [createSuccess, setCreateSuccess] = useState(true);
     const [joinSuccess, setJoinSuccess] = useState(true);
 
-    const createSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    const createSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
+
+        // Create a room doc with an empty messages collection
     };
 
-    const joinSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    const joinSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
+
+        const docRef = doc(db, "rooms", joinId);
+        try {
+            const docSnap = await getDoc(docRef);
+            console.log(docSnap);
+            if (docSnap.exists()) {
+                console.log(docSnap.data);
+            }
+        } catch (error) {
+            console.error(error);
+        }
     };
 
     return (
         <div>
             <form onSubmit={createSubmit}>
-                <input type="text" />
+                <input type="text" name="createId" onChange={(e) => setCreateId(e.target.value)} />
                 <button>Create</button>
             </form>
             <form onSubmit={joinSubmit}>
-                <input type="text" />
+                <input type="text" name="joinId" onChange={(e) => setJoinId(e.target.value)} />
                 <button>Join</button>
             </form>
         </div>
