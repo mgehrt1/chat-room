47d26eae5ce4a2c15f4d5218df5ce0144e7b4e47
Matthew Gehrt <mgehrt@hbs.net>
2023-08-02 16:06:11 -0500

    message sizing

--- components/ChatRoom.tsx
+++ components/ChatRoom.tsx
@@ -31,24 +31,28 @@ export default function ChatRoom({ roomId }: Props) {
     let exists: boolean | undefined = rooms?.docs.some((doc) => doc.id == roomId);
 
     return (
-        <div>
+        <>
             {exists ? (
-                <div className="max-w-screen-xl px-5">
-                    <h1>
-                        Join with code: <span className="text-red-500">{roomId}</span> or with this{" "}
-                        <Link href={`/room/${roomId}`} className="text-blue-500">
-                            link
-                        </Link>
-                    </h1>
-                    <div>{messages && messages.docs.map((doc) => <Message key={doc.id} msg={doc.data()} />)}</div>
-                    <form onSubmit={sendMessage} className="flex gap-2 w-full mt-10">
-                        <input type="text" name="text" placeholder="Message" value={text} onChange={(e) => setText(e.target.value)} className="outline-0 border-2 rounded-3xl placeholder:text-xl w-full py-2 px-4" />
-                        <button className="bg-green-500 rounded-3xl text-xl py-2 px-14">Send</button>
-                    </form>
-                </div>
+                <>
+                    <div className="flex justify-center items-center bg-green-400 text-lg w-full h-10">
+                        <h1>
+                            Let others join with code <span className="text-red-500">{roomId}</span> or with this link{" "}
+                            <Link href={`/room/${roomId}`} className="text-blue-500">
+                                {window.location.href}
+                            </Link>
+                        </h1>
+                    </div>
+                    <div className="w-full h-full xl:px-[12rem] lg:px-[6rem]">
+                        <div className="w-full h-full px-5">{messages && messages.docs.map((doc) => <Message key={doc.id} msg={doc.data()} />)}</div>
+                        <form onSubmit={sendMessage} className="flex gap-2 w-full mt-10 px-5">
+                            <input type="text" name="text" placeholder="Message" value={text} onChange={(e) => setText(e.target.value)} className="outline-0 border-2 rounded-3xl placeholder:text-xl w-full py-2 px-4" />
+                            <button className="bg-green-500 rounded-3xl text-xl py-2 px-14">Send</button>
+                        </form>
+                    </div>
+                </>
             ) : (
                 <h1>Room doesn't exist</h1>
             )}
-        </div>
+        </>
     );
 }
--- components/Message.tsx
+++ components/Message.tsx
@@ -16,13 +16,17 @@ export default function Message({ msg }: Props) {
 
     return isCurrentUserMessage ? (
         <div className="flex justify-end items-center my-2">
-            <p className="bg-light-blue rounded-3xl text-xl text-white-blue py-2 px-4 ml-[26rem]">{text}</p>
+            <div className="flex justify-end w-3/5">
+                <p className="bg-light-blue rounded-3xl text-xl text-white-blue py-2 px-4">{text}</p>
+            </div>
             <img src={photoURL} className="rounded-full w-10 h-10 ml-2" />
         </div>
     ) : (
         <div className="flex justify-start items-center my-2">
             <img src={photoURL} className="rounded-full w-10 h-10 mr-2" />
-            <p className="bg-gray-200 rounded-3xl text-xl py-2 px-4 mr-[26rem]">{text}</p>
+            <div className="flex justify-start w-2/3">
+                <p className="bg-gray-200 rounded-3xl text-xl py-2 px-4">{text}</p>
+            </div>
         </div>
     );
 }
