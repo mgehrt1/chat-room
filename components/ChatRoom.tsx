173f8ad5397f94466e844dc09b8c0752300d863e
Matthew Gehrt <mgehrt@hbs.net>
2023-08-02 10:44:18 -0500

    chat room styling

--- components/ChatRoom.tsx
+++ components/ChatRoom.tsx
@@ -1,8 +1,9 @@
 import { db, auth } from "@/config/firebase";
-import { addDoc, collection, serverTimestamp } from "firebase/firestore";
+import { addDoc, collection, limit, orderBy, query, serverTimestamp } from "firebase/firestore";
 import { useState } from "react";
 import { useCollection } from "react-firebase-hooks/firestore";
 import Message from "./Message";
+import Link from "next/link";
 
 interface Props {
     roomId: string;
@@ -11,7 +12,7 @@ interface Props {
 export default function ChatRoom({ roomId }: Props) {
     const [rooms] = useCollection(collection(db, "rooms"));
     const [text, setText] = useState("");
-    const [messages] = useCollection(collection(db, `rooms/${roomId}/messages`));
+    const [messages] = useCollection(query(collection(db, `rooms/${roomId}/messages`), orderBy("createdAt"), limit(50)));
     const currentUser = auth.currentUser;
 
     const sendMessage = async (e: React.FormEvent<HTMLFormElement>) => {
@@ -32,13 +33,17 @@ export default function ChatRoom({ roomId }: Props) {
     return (
         <div>
             {exists ? (
-                <div>
-                    <h1>Chat</h1>
-                    {messages && messages.docs.map((doc) => <Message key={doc.id} msg={doc.data()} />)}
-
-                    <form onSubmit={sendMessage}>
-                        <input type="text" name="text" placeholder="Message" value={text} onChange={(e) => setText(e.target.value)} className="outline-0 border-2 rounded-xl pl-2" />
-                        <button>Send</button>
+                <div className="max-w-screen-xl px-5">
+                    <h1>
+                        Join with code: <span className="text-red-500">{roomId}</span> or with this{" "}
+                        <Link href={`/room/${roomId}`} className="text-blue-500">
+                            link
+                        </Link>
+                    </h1>
+                    <div>{messages && messages.docs.map((doc) => <Message key={doc.id} msg={doc.data()} />)}</div>
+                    <form onSubmit={sendMessage} className="flex gap-2 w-full mt-10">
+                        <input type="text" name="text" placeholder="Message" value={text} onChange={(e) => setText(e.target.value)} className="outline-0 border-2 rounded-3xl placeholder:text-xl w-full py-2 px-4" />
+                        <button className="bg-green-500 rounded-3xl text-xl py-2 px-14">Send</button>
                     </form>
                 </div>
             ) : (
--- components/Message.tsx
+++ components/Message.tsx
@@ -9,20 +9,20 @@ interface Props {
 
 export default function Message({ msg }: Props) {
     const [user] = useAuthState(auth);
-    const { text, createdAt, uid, photoURL }: DocumentData = msg;
+    const { text, uid, photoURL }: DocumentData = msg;
 
     const userId: string | null = user?.uid.toString() ?? null;
     const isCurrentUserMessage = uid === userId;
 
     return isCurrentUserMessage ? (
-        <div className="flex justify-end items-center">
-            <p className="text-xl">{text}</p>
-            <img src={photoURL} className="rounded-full w-8 h-8 ml-2" />
+        <div className="flex justify-end items-center my-2">
+            <p className="bg-light-blue rounded-3xl text-xl text-white-blue py-2 px-4 ml-[26rem]">{text}</p>
+            <img src={photoURL} className="rounded-full w-10 h-10 ml-2" />
         </div>
     ) : (
-        <div className="flex justify-start items-center">
-            <img src={photoURL} className="rounded-full w-8 h-8 mr-2" />
-            <p className="text-xl">{text}</p>
+        <div className="flex justify-start items-center my-2">
+            <img src={photoURL} className="rounded-full w-10 h-10 mr-2" />
+            <p className="bg-gray-200 rounded-3xl text-xl py-2 px-4 mr-[26rem]">{text}</p>
         </div>
     );
 }
