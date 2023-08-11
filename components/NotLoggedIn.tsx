0586ad65e9538a026c98cd392062a17ebe55d1b5
Matthew Gehrt <mgehrt@hbs.net>
2023-08-11 13:51:51 -0500

    dont need this file anymore

--- components/NotLoggedIn.tsx
+++
@@ -1,7 +0,0 @@
-export default function NotLoggedIn() {
-    return (
-        <div className="flex justify-center w-full h-full">
-            <p className="mt-40 text-5xl text-navy font-bold">You must be logged in to access chat rooms</p>
-        </div>
-    );
-}
